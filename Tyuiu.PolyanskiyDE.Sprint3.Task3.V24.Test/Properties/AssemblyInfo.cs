5be5f899f7303a6e371707feb879fdb69b6cb387
Tyuiu.PolyanskiyDE <danilp.2005@mail.ru>
2023-10-22 22:44:57 +0500

    Task3

---
+++ Tyuiu.PolyanskiyDE.Sprint3.Task3.V24.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PolyanskiyDE.Sprint3.Task3.V24.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PolyanskiyDE.Sprint3.Task3.V24.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fc10e689-150d-49fd-9e0f-6ab0b533c814")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PolyanskiyDE.Sprint3.Task3.V24/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PolyanskiyDE.Sprint3.Task3.V24.Lib;
+
+namespace Tyuiu.PolyanskiyDE.Sprint3.Task3.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Полянский Д. Э. | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры                                   *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Полянский Данил Эдуардович | АСОиУБ-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл foreach заменить буквы                                   *");
+            Console.WriteLine("* g  на * в строке: gft hggt ntg                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string value = "gft hggt ntg";
+            char replaceable = 'g';
+            char replacement = '*';
+
+            Console.WriteLine("Исходная строка = " + value);
+            Console.WriteLine("Символ, который нужно заменить = " + replaceable);
+            Console.WriteLine("Символ, на который нужно заменить = " + replacement);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Новая строка = " + ds.ReplaceCharInString(value, replaceable, replacement));
+
+            Console.ReadKey();
+
+        }
+    }
+}
